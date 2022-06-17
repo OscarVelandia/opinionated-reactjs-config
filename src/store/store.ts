65940f7d8a0cd1031eab9925fa184bc13f06b1d8
Oscar Velandia <oscar.daniel.v.s.235@gmail.com>
2022-06-16 21:52:21 -0500

    setUp listener in store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { counterReducer } from '@features/counter';
 import { pokemonApi } from '@services';
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
 
 export const store = configureStore({
   reducer: {
@@ -11,6 +12,10 @@ export const store = configureStore({
     return getDefaultMiddleware().concat(pokemonApi.middleware);
   },
 });
+
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+setupListeners(store.dispatch)
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
